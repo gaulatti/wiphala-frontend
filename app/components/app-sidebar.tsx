2d71769586ba9336e898fa723533391e782acbc1
Javier Godoy Núñez <javier@oxozon.io>
2025-03-15 12:08:54 -0400

    landed: Logo and fonts

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,11 +1,37 @@
 import { AudioWaveform, Command, Frame, GalleryVerticalEnd, Map, PieChart, SquareTerminal } from 'lucide-react';
 import * as React from 'react';
+import { NavLink } from 'react-router';
 
+import styled from '@emotion/styled';
 import { NavMain } from '~/components/nav-main';
 import { NavProjects } from '~/components/nav-projects';
 import { NavUser } from '~/components/nav-user';
 import { TeamSwitcher } from '~/components/team-switcher';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } from '~/components/ui/sidebar';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail, useSidebar } from '~/components/ui/sidebar';
+
+/**
+ * A styled component that represents the logo in the sidebar.
+ * It is based on the `NavLink` component and has specific styles applied to it.
+ *
+ * Styles applied:
+ * - Height: 66px
+ * - Display: inline-block
+ * - Padding-right: 1rem
+ * - Line-height: 66px
+ * - Font-size: 2rem
+ * - Text-align: center
+ * - Font-family: 'Comodo', sans-serif
+ */
+const Logo = styled(NavLink)`
+  height: 66px;
+  display: inline-block;
+  padding-right: 1rem;
+  line-height: 66px;
+  font-size: 2rem;
+  text-align: center;
+  font-family: 'Comodo', sans-serif;
+`;
+
 
 // This is sample data.
 const data = {
@@ -59,9 +85,12 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { state } = useSidebar()
+
   return (
     <Sidebar collapsible='icon' {...props}>
       <SidebarHeader>
+        {state !== 'collapsed' && <Logo to='/'>wiphala</Logo>}
         <TeamSwitcher teams={data.teams} />
       </SidebarHeader>
       <SidebarContent>
